79fe3064a743700d84b4fcfc42cb061b66d21bdd
Adam Purdon <adampurdondev@gmail.com>
2020-12-26 14:36:41 +0200

    :bug: git worktree no staging server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
+// server.js
 const express = require('express');
+const path = require("path");
+
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
 
-// DB coniguration
+// DB Configuration
 require('./src/database');
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+const CLIENT_BUILD_PATH = path.join(__dirname, "../client/build");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+// Static files
+app.use(express.static(CLIENT_BUILD_PATH));
+
+// Server React Client
+app.get("/", function(req, res) {
+  res.sendFile(path.join(CLIENT_BUILD_PATH , "index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+app.listen(PORT, function () {
+    console.log(`Server Listening on ${PORT}`);
 });
